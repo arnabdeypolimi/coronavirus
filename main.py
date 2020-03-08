775a63a5c5875ce56ec56254ddf51194e772a8ea
Arnab Dey <32412133+arnabdeypolimi@users.noreply.github.com>
2020-03-08 19:47:38 +0100

    pic

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ time.sleep(10)
 data=driver.find_element_by_xpath('//*[@id="main_table_countries"]/tbody/tr')
 print(data)
 data=[[0,0,0,0,0,0,0,0]]
-df=pd.DataFrame(data,columns=['country', 'total', 'new case', 'death', 'new_death', 'active_cases', 'recovered', 'critical'])
+df=pd.DataFrame(data,columns=['country', 'total', 'new case', 'death', 'new_death','recovered', 'active_cases',  'critical'])
 for tr in driver.find_elements_by_xpath('//*[@id="main_table_countries"]/tbody/tr'):
     tds=tr.find_elements_by_tag_name('td')
     temp=[]
